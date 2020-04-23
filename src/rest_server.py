aadb60ef2c258c82e45066e2e4b5e42e4e52494a
Richard Bieringa <richardbieringa@gmail.com>
2020-04-23 16:11:14 +0200

    updated every response to return JSON

--- src/rest_server.py
+++ src/rest_server.py
@@ -27,6 +27,7 @@ if os.environ.get('FLASK_ENV') is not None and os.environ.get('FLAS_ENV') == 'PR
 app = Flask(__name__)
 
 app.config['JWT_SECRET_KEY'] = 'default_secret_key'
+app.config['JWT_COOKIE_CSRF_PROTECT'] = False
 app.config['JWT_TOKEN_LOCATION'] = ['cookies']
 
 # JWT Secret 
@@ -96,36 +97,40 @@ def login():
         else:
             return jsonify({"message": "Invalid password"}), 403
 
-    return jsonify({"message": "User nor found"}), 403
+    return jsonify({"message": "User not found"}), 403
 
-@app.route('/', methods=['GET', 'POST', 'DELETE'])
-def full():
-    # Should list all links
+
+@app.route('/', methods=['GET'])
+def get_all():
     if request.method == 'GET':
         return jsonify(url_map), 200
 
+@app.route('/', methods=['POST', 'DELETE'])
+@jwt_required
+def full():
+    full_url = request.form.get('url')
+    if not full_url:
+        return jsonify({"message": "Missing url parameter"}), 400
+
     # Should create a link between full URL/shortened URL
     if request.method == 'POST':
-        full_url = request.form.get('url')
-
         #  REGEX check for valid URL
         if not URL_RE.match(full_url):
-            return 'URL posted is not a valid URL', 400
+            return jsonify({"message": "URL posted is not a valid URL", "url": full_url}), 400
 
         #  Check if an existing shortened URL was found and return it
         for key, value in url_map.items():
             if full_url == value:
-                return 'Shortened URL already existed: /{}'.format(key), 201
+                return jsonify({"message": "Shortened URL already existed", "url": full_url, "short_url": key}), 201
 
         # generate new short URL and save it in memory
         new_url = generate_short_url()
         url_map[new_url] = full_url
 
-        return 'Shortened URL: /{}'.format(new_url), 201
+        return jsonify({"message": "Created URL link", "url": full_url, "short_url": new_url}), 201
 
     # Should delete a link between full URL/shortened URL
     if request.method == 'DELETE':
-        full_url = request.form.get('url')
 
         # Check if a match is found, if so delete
         for key, value in url_map.items():
@@ -134,9 +139,10 @@ def full():
                 return 204
 
         # No links were found matchin the full URL
-        return 'Did not found a link containing the URL: {}'.format(full_url), 404
+        return jsonify({"message": "Did not found a link for the given URL", "url": full_url}), 201
 
 @app.route('/<id>', methods=['GET', 'PUT', 'DELETE'])
+@jwt_required
 def shortened(id):
     # Check if shortened URL is available and redirect
     if request.method == 'GET':
@@ -145,31 +151,34 @@ def shortened(id):
             return redirect(url_map[id])
 
         # Error if not found
-        return 'Could not find a matching URL for the provided shortened URL /{}'.format(id), 404
+        return jsonify({"message": "Could not find a matching URL", "short_url": id}), 201
 
     # idem potent update of short url
     if request.method == 'PUT':
         full_url = request.form.get('url')
 
+        if not full_url:
+            return jsonify({"message": "Missing url parameter"}), 400
+
         # Couldn't find a link
         if id not in url_map:
-            return 'Could not find a matching URL for the provided shortened URL /{}'.format(id), 404
+            return jsonify({"message": "Could not find a matching URL for the provided short url", "short_url": id}), 404
 
         #  REGEX check for valid URL
         if not URL_RE.match(full_url):
-            return 'URL posted is not a valid URL', 400
+            return jsonify({"message": "URL posted is not a valid URL", "url": full_url}), 400
 
         # If found update to new url value
         for key, value in url_map.items():
             if id == key:
                 url_map[key] = full_url
-                return 'Updated link {} -> {} to {} -> {}'.format(key, value, key, full_url), 200
+                return jsonify({"message": "Udated URL link", "url": full_url, "short_url": key}), 200
 
     # Should delete a link between full URL/shortened URL
     if request.method == 'DELETE':
         # Couldn't find a link
         if id not in url_map:
-            return 'Could not find a matching URL for the provided shortened URL /{}'.format(id), 404
+            return jsonify({"message": "Could not find a matching URL for the provided shortened URL", "short_url": key}), 404
 
         # Check if a match is found, if so delete
         for key, value in url_map.items():
